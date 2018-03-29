283ef6adae5aedf5e5fd05185703b1c939060877
Rhys Arkins <rhys@arkins.net>
2018-03-29 12:04:19 +0200

    Remove date and alphabetical tests

--- test.js
+++ test.js
@@ -1,14 +1,10 @@
 const fs = require("fs");
 const parse = require("csv-parse/lib/sync");
-const { DateTime } = require("luxon");
 
 const list = fs.readFileSync("list.csv", "utf8");
 const listParsed = parse(list);
 listParsed.shift(); // header row
 
-const now = DateTime.local();
-
-let previous = "";
 let seen = [];
 listParsed.forEach(row => {
   const [username, expiry] = row;
@@ -17,20 +13,4 @@ listParsed.forEach(row => {
     process.exit(1);
   }
   seen.push(username);
-  if (username < previous) {
-    console.error(`List is not in alphabetical order: ${username}`);
-    process.exit(1);
-  }
-  previous = username;
-  const expiryDate = DateTime.fromISO(expiry);
-  if (!expiryDate.isValid) {
-    console.error(`Cannot parse expiry: ${expiry}`);
-    process.exit(1);
-  }
-  if (expiryDate > now.plus({ months: 12, weeks: 1 })) {
-    console.error(
-      `${username} expiry "${expiry}" cannot be more than 1 year in advance`
-    );
-    process.exit(1);
-  }
 });
